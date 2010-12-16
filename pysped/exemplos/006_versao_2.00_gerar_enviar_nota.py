4a45dee42bd4eccc09c4eac4e9368f8d75dc4af0
Alan Justino <alan.justino@yahoo.com.br>
2010-12-16 14:40:51 -0200

    Exemplos podem ser depurados com Winpdb

--- pysped/exemplos/006_versao_2.00_gerar_enviar_nota.py
+++ pysped/exemplos/006_versao_2.00_gerar_enviar_nota.py
@@ -4,16 +4,18 @@ from pysped.nfe import ProcessadorNFe
 from pysped.nfe.webservices_flags import *
 from pysped.nfe.manual_401 import *
 from datetime import datetime
+from os.path import abspath, dirname
 
+FILE_DIR = abspath(dirname(__file__))
 
 if __name__ == '__main__':
     p = ProcessadorNFe()
     p.versao              = u'2.00'
     p.estado              = u'SP'
     # arquivo 'certificado_caminho.txt' deve conter o caminho para o 'certificado.pfx'
-    p.certificado.arquivo = open('certificado_caminho.txt').read().strip()
+    p.certificado.arquivo = open(FILE_DIR+'/certificado_caminho.txt').read().strip()
     # arquivo 'certificado_senha.txt' deve conter a senha para o 'certificado.pfx'
-    p.certificado.senha   = open('certificado_senha.txt').read().strip()               
+    p.certificado.senha   = open(FILE_DIR+'/certificado_senha.txt').read().strip()               
     p.salva_arquivos      = True
     p.contingencia_SCAN   = False
     p.caminho = u'' 
