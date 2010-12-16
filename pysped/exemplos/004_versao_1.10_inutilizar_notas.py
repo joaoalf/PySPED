d2475fcf281bd4a5deaf7c9b4a221b7d5dbfda8f
Alan Justino <alan.justino@yahoo.com.br>
2010-12-16 14:40:51 -0200

    Exemplos podem ser depurados com Winpdb
    --HG--
    extra : rebase_source : 78c3cd608d6b37d09b6c51cecfdce847e0d230e4

--- pysped/exemplos/004_versao_1.10_inutilizar_notas.py
+++ pysped/exemplos/004_versao_1.10_inutilizar_notas.py
@@ -2,16 +2,18 @@
 
 from pysped.nfe import ProcessadorNFe
 from pysped.nfe.webservices_flags import *
+from os.path import abspath, dirname
 
+FILE_DIR = abspath(dirname(__file__))
 
 if __name__ == '__main__':
     p = ProcessadorNFe()
     p.versao              = u'1.10'
     p.estado              = u'SP'
     # arquivo 'certificado_caminho.txt' deve conter o caminho para o 'certificado.pfx'
-    p.certificado.arquivo = open('certificado_caminho.txt').read().strip()
+    p.certificado.arquivo = open(FILE_DIR+'/certificado_caminho.txt').read().strip()
     # arquivo 'certificado_senha.txt' deve conter a senha para o 'certificado.pfx'
-    p.certificado.senha   = open('certificado_senha.txt').read().strip()                 
+    p.certificado.senha   = open(FILE_DIR+'/certificado_senha.txt').read().strip()                 
     p.salva_arquivos      = True
     p.contingencia_SCAN   = False
     p.caminho = u'' 
