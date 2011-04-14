30c6e7696ec034eb45721d7aa61da4a2b0ef6065
Alan Justino <alan.justino@yahoo.com.br>
2011-04-14 02:09:10 -0300

    [WIP] Refazendo um merge mal sucedido

--- pysped/nfe/processador_nfe.py
+++ pysped/nfe/processador_nfe.py
@@ -52,6 +52,9 @@ from manual_401 import ConsStatServ_200, RetConsStatServ_200
 from danfe.danferetrato import *
 from StringIO import StringIO
 
+# Vezes a tentar novamente caso o recibo de lote retorne "Em processamento"
+TENTATIVAS_MAXIMAS = 5
+
 
 class ProcessoNFe(object):
     def __init__(self, webservice=0, envio=u'', resposta=u''):
@@ -590,10 +593,14 @@ class ProcessadorNFe(object):
             # Deu certo?
             #
             if ret_envi_nfe.cStat.valor == u'103':
-                time.sleep(ret_envi_nfe.infRec.tMed.valor * 3) # Espere o processamento antes de consultar o recibo
-                proc_recibo, novos_arquivos = self.consultar_recibo(ambiente=ret_envi_nfe.tpAmb.valor, numero_recibo=ret_envi_nfe.infRec.nRec.valor)
-                processos['recibo'] = proc_recibo, novos_arquivos
-                yield proc_recibo
+                time.sleep(ret_envi_nfe.infRec.tMed.valor * 1.3) # Espere o processamento antes de consultar o recibo
+                proc_recibo = self.consultar_recibo(ambiente=ret_envi_nfe.tpAmb.valor, numero_recibo=ret_envi_nfe.infRec.nRec.valor)
+                tentativas = 0
+                while proc_recibo.resposta.cStat.valor == '105' and tentativas < TENTATIVAS_MAXIMAS: # Ainda em processamento
+                    time.sleep(ret_envi_nfe.infRec.tMed.valor * 1.5) # Espere o processamento antes de consultar o recibo
+                    tentativas += 1
+                    #print 'tentativas:', tentativas
+                    proc_recibo = self.consultar_recibo(ambiente=ret_envi_nfe.tpAmb.valor, numero_recibo=ret_envi_nfe.infRec.nRec.valor)
 
                 # Montar os processos das NF-es
                 dic_protNFe = proc_recibo.resposta.dic_protNFe
