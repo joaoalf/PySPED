5d0479959b2e2f3fa875f826c39d67d5f0b8a203
João Alfredo Gama Batista <ermitaoj@gmail.com>
2011-08-25 13:25:58 -0300

    Let's make some eggs.

--- setup.py
+++ setup.py
@@ -1,2 +1,20 @@
+# -*- coding: utf-8 -*-
 __author__ = 'joaoalf'
-  
+
+from setuptools import setup
+
+setup(
+    name = u'PySPED',
+    description = u'Sistema Público de Escrituração Digital em Python',
+    author = u'Aristides Caldeira',
+    author_email = u'aristides.caldeira@taugars.com.br',
+    package_dir={'': 'pysped'},
+    include_package_data=True,
+    install_requires=['setuptools',
+                      'lxml',
+                      'suds',
+                      'PyOpenSSL',
+                      'PyXMLSec',
+                      'reportlab',
+                      'Geraldo'],
+)
