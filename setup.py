bd1581b4365ab8eb42324b098d652d2c1e31c9e1
João Alfredo Gama Batista <ermitaoj@gmail.com>
2011-08-25 20:42:42 -0300

    Typo error.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description = u'Sistema Público de Escrituração Digital em Python',
     author = u'Aristides Caldeira',
     author_email = u'aristides.caldeira@taugars.com.br',
-    packages = find_packges('pysped'),
+    packages = find_packages('pysped'),
     include_package_data=True,
     zip_safe = False,
     dependency_links = ['https://github.com/joaoalf/geraldo/tarball/master#egg=Geraldo-0.1dev_joaoalf_branch'],
