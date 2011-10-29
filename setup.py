b257309c396c26384fad6ac74e216156dac7e022
João Alfredo Gama Batista <ermitaoj@gmail.com>
2011-10-29 14:41:01 -0300

    Transform it into a proper package eggable and everything.

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
-__author__ = 'joaoalf'
 
+import os
 from setuptools import setup, find_packages
 
 setup(
     name = u'PySPED',
+    version = '0.1dev_joaoalf_branch',
     description = u'Sistema Público de Escrituração Digital em Python',
     author = u'Aristides Caldeira',
     author_email = u'aristides.caldeira@taugars.com.br',
-    packages = ['pysped',
-                'pysped.nfe',
-                'pysped.nfe.danfe',
-                'pysped.nfe.manual_300',
-                'pysped.nfe.manual_401',
-                'pysped.nfe.manual_401.schema',
-                'pysped.nfe.manual_401.schema.pl_006g',
-                'pysped.xml_sped',
-                'pysped.relato_sped'],
+    packages = find_packages(),
+    package_data = {'pysped.nfe.manual_401': [os.path.join('schema', 'pl_006g', '*')],
+                    'pysped.nfe.danfe': [os.path.join('fonts', '*')],},
     include_package_data=True,
-    zip_safe = False,
+    zip_safe = True,
     dependency_links = ['https://github.com/joaoalf/geraldo/tarball/master#egg=Geraldo-0.1dev_joaoalf_branch'],
     install_requires=['setuptools',
                       'lxml',
